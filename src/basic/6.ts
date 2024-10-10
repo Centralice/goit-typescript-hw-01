3c8ba2bbfc3412486c45b9a49d471cd3548bd2c8
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-10 18:54:49 +0200

    6 basics

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -1,17 +1,40 @@
-Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
 
-const mango = {
-    name: 'Mango',
-    age: 30,
-    email: 'john@example.com',
-    address: {
-        city: 'New York',
-        country: 'USA'
-    }
+// const mango = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com',
+//     address: {
+//         city: 'New York',
+//         country: 'USA'
+//     }
+// };
+
+// const poly = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com'
+// };
+
+interface IUser {
+  name: string;
+  age: number;
+  email: string;
+  address?: { city: string; country: string };
+}
+
+const mango: IUser = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
 };
 
-const poly = {
-    name: 'Mango',
-    age: 30,
-    email: 'john@example.com'
+const poly: IUser = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
 };
